1b140c56b14922845d5dd9642a5920bc1bcde63b
rheber <rheber@rediffmail.com>
2022-07-17 14:58:07 +1000

    initial commit

---
+++ src/mjtk/core.clj
@@ -0,0 +1,8 @@
+(ns mjtk.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (if args 
+    (println (first args))))
---
+++ test/mjtk/core_test.clj
@@ -0,0 +1,7 @@
+(ns mjtk.core-test
+  (:require [clojure.test :refer :all]
+            [mjtk.core :refer :all]))
+
+(deftest a-test
+  (testing "example"
+    (is (= 42 42))))
